e4e7b293208070c4eaa7ca03556fb50b71312587
kamal126 <kamalchanda265@gmail.com>
2025-06-04 22:08:13 +0530

    Dijsktra Algo using min-heap

--- Graph/15_DijsktraAlgo/02_usingHeap.cpp
+++ Graph/15_DijsktraAlgo/02_usingHeap.cpp
@@ -1,10 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 class Solution {
   public:
     vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
-        
-        // 1. create adj list 
+          // 1. create adj list 
         vector<pair<int,int>>adj[V];
         for(auto& edge : edges){
             int u = edge[0];
@@ -15,40 +15,35 @@ class Solution {
             adj[v].push_back({u,wt});
         }
         
-        
         // 2. create a explored and distance(src->dest) array
         vector<bool>explored(V,0);
         vector<int>dist(V,INT_MAX);
+        // min heap
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
         
-        
-        // 3. dist of src to src is 0
         dist[src]=0;
+        pq.push({0,src});
         
-        // 6. reapeat same work all vertex
-        int count = V;
-        while(count--){
-        //4.find minimum distance and explored mark 1
-        int node = -1, wt = INT_MAX;
-        for(int i=0;i<V;i++){
-            if(!explored[i] && dist[i]<wt){
-                node = i;
-                wt = dist[i];
-            }
-        }
-        explored[node] = 1;
-        
-        // 5. explore or visit node's neighbours
-        for(int j=0;j<adj[node].size();j++){
+        while(!pq.empty()){
+            int node = pq.top().second;
+            pq.pop();
+            
+            if(explored[node]) continue;
+            
+            explored[node] = 1;
+            
+            for(int j=0;j<adj[node].size();j++){
             int neighbour = adj[node][j].first;
             int wt = adj[node][j].second;
             // a. check not explored neighbor alredy then dist[node] + wt < dist[neighbour] then dist[neghbour] = dist[node]+wt
             if(!explored[neighbour] && dist[node]+wt < dist[neighbour]){
                 dist[neighbour] = dist[node]+wt;
+                
+                pq.push({dist[neighbour], neighbour});
             }
         }// end for 
-        }// end while loop
+        }
         
         return dist;
-        
     }
 };
