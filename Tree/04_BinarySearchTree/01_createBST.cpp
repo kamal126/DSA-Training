179c25146c6bc11f68b987ebe901fb014ff0148f
kamal126 <kamalchandra265@gmail.com>
2025-07-21 22:55:00 +0530

    create BST

---
+++ Tree/04_BinarySearchTree/01_createBST.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *left, *right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+Node* BST(int x){
+    if(x==-1) return NULL;
+    
+    Node *root = new Node(x);
+
+    
+    
+}
+
+int main(){
+
+}
