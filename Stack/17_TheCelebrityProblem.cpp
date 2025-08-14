6b3f88dd3398577f011968235b45441ec53b52cf
kamal126 <kamalchandra265@gmail.com>
2025-08-14 22:47:50 +0530

    Add implementation of the Celebrity Problem solution using stack

---
+++ Stack/17_TheCelebrityProblem.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int celebrity(vector<vector<int> >& mat) {
+        stack<int>st;
+
+        int n = mat.size();
+
+        for (int i = 0; i < n; i++)
+            st.push(i);
+        
+            while(st.size() > 1){
+                int a = st.top(); st.pop();
+                int b = st.top(); st.pop();
+
+                mat[a][b]? st.push(b) : st.push(a);
+            }
+
+            int c = st.top(); st.pop();
+
+            for(int i=0;i<n;i++){
+                if(i==c) continue;
+
+                if(mat[c][i] || !mat[i][c])
+                    return -1;
+            }
+
+            return c;
+    }
+};
