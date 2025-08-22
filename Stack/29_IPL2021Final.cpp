a6adb737104572defe4fcb9474f3bf5428879c4f
kamal126 <kamalchandra265@gmail.com>
2025-08-23 00:15:19 +0530

    Second Approach Valid parenthesis max length in substring

--- Stack/29_IPL2021Final.cpp
+++ Stack/29_IPL2021Final.cpp
@@ -22,4 +22,36 @@ class Solution {
 
         return maxLen;
     }
+    
+    int findMaxLen2(string s) {
+        int n = s.size();
+        int open=0, close=0;
+        int maxLen = 0;
+        // left to right
+        for(char ch : s){
+            if(ch == '(') open++;
+            else close++;
+
+            if(open==close) 
+                maxLen = max(maxLen, 2*close);
+            
+            if(close > open) open=close=0;
+        }
+
+        // initial state open and close = 0
+        open=close=0;
+        // right to left
+
+        for(int i=n-1;i>=0;i--){
+            if(s[i]==')') close++;
+            else open++;
+
+            if(open==close)
+                maxLen = max(maxLen, 2*open);
+            
+            if(open > close) open=close = 0;
+        }
+
+        return maxLen;
+    }
 };
