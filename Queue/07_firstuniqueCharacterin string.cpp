cbe70f70ac77524d73e5c21935296e492b0c166f
kamal126 <kamalchandra265@gmail.com>
2025-08-25 22:19:59 +0530

    second approach'

--- Queue/07_firstuniqueCharacterin string.cpp
+++ Queue/07_firstuniqueCharacterin string.cpp
@@ -18,4 +18,25 @@ public:
 
         return -1;
     }
+
+    int firstUniqChar1(string s) {
+        vector<int>freq(26,0);
+        queue<int>q;
+
+        for(int i=0;i<s.size(); i++){
+            freq[s[i]-'a']++;
+            q.push(i);
+        }
+
+        while(!q.empty()){
+            int idx = q.front();
+
+            if(freq[s[idx]-'a']==1) return idx;
+
+            q.pop();
+        }
+
+        return -1;
+    }
+
 };
