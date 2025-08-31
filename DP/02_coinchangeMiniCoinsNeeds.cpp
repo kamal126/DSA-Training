fb9a106e0c36b4f351bc66387c99a10ebede7235
kamal126 <kamalchandra265@gmail.com>
2025-08-31 19:58:09 +0530

    add all solution and test cases

--- DP/02_coinchangeMiniCoinsNeeds.cpp
+++ DP/02_coinchangeMiniCoinsNeeds.cpp
@@ -21,14 +21,83 @@ public:
             return dp[idx][sum] = min(1 + find(idx, coins, sum - coins[idx], dp), find(idx - 1, coins, sum, dp));
     }
 
+    // ================== space optimization =======================
     int minCoins(vector<int> &coins, int sum)
     {
         int n = coins.size();
 
-        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, -1));
+        vector<int> dp(sum + 1, 1e9);
 
-        int ans = find(n - 1, coins, sum, dp);
+        dp[0] = 0;
 
-        return ans >= 1e9 ? -1 : ans;
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j <= sum; j++)
+            {
+                if (coins[i] <= j)
+                {
+                    dp[j] = min(1 + dp[j - coins[i]], dp[j]);
+                }
+            }
+        }
+
+        return dp[sum] >= 1e9 ? -1 : dp[sum];
     }
+
+
+    // =============== Bottom Up =====================================
+    // int minCoins(vector<int> &coins, int sum){
+    //     int n = coins.size();
+
+    //     vector<vector<int>> dp(n + 1, vector<int>(sum + 1, 1e9));
+
+    //     for(int i=0;i<=n;i++) dp[i][0] = 0;
+
+    //     for(int i=1;i<=n;i++){
+    //         for(int j=1;j<=sum;j++){
+    //             if(coins[i]>j){
+    //                 dp[i][j] = dp[i-1][j];
+    //             }else{
+    //                 dp[i][j] = min(1+dp[i][j-coins[i-1]], dp[i-1][j]);
+    //             }
+    //         }
+    //     }
+
+    //     return dp[n][sum] >= 1e9 ? -1 : dp[n][sum];
+    // }
+
+
+
+    // ======================= recursive DP ============================
+    // int minCoins(vector<int> &coins, int sum)
+    // {
+    //     int n = coins.size();
+
+    //     vector<vector<int>> dp(n + 1, vector<int>(sum + 1, -1));
+
+    //     int ans = find(n - 1, coins, sum, dp);
+
+    //     return ans >= 1e9 ? -1 : ans;
+    // }
 };
+
+int main(){
+    Solution obj;
+
+    vector<int>coins = {20,10,5}; int sum = 30;
+    vector<int>coins1 = {2,4,17,2,1,19}; int sum1 = 39;
+    vector<int>coins2 = {9,6,5,1}; int sum2 = 19;
+    vector<int>coins3 = {2, 5, 7, 12, 15, 16}; int sum3 = 1;
+
+    int ans1 = obj.minCoins(coins, sum);
+    int ans2 = obj.minCoins(coins1, sum1);
+    int ans3 = obj.minCoins(coins2, sum2);
+    int ans4 = obj.minCoins(coins3, sum3);
+
+    cout<<"ans1 :"<<ans1<<endl;
+    cout<<"ans2 :"<<ans2<<endl;
+    cout<<"ans3 :"<<ans3<<endl;
+    cout<<"ans4 :"<<ans4<<endl;
+
+    return 0;
+}
