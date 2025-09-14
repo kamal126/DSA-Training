5752e1e9e6d665a4edfee798eb4c4c7d3bdb734b
kamal126 <kamalchandra265@gmail.com>
2025-09-14 23:08:14 +0530

    fix: update longest repeating subsequence function to use memoization

--- DP/08_longestRepeatingSubsequence.cpp
+++ DP/08_longestRepeatingSubsequence.cpp
@@ -6,24 +6,32 @@ class Solution {
   public:
   int n;
     
-    int find(int i, int j, string &s1, string &s2){
+    int find(int i, int j, string &s1, string &s2, vector<vector<int>>&dp){
         if(i==n || j==n) return 0;
         
+        if(dp[i][j]!=-1) return dp[i][j];
+        
         if(i!=j && s1[i]==s2[j]){
-            return max(1+find(i+1, j+1, s1, s2), 
-            max(find(i, j+1, s1, s2), find(i+1, j, s1, s2)));
+            return dp[i][j] = max(1+find(i+1, j+1, s1, s2, dp), 
+            max(find(i, j+1, s1, s2, dp), find(i+1, j, s1, s2, dp)));
         }else{
-            return max(find(i, j+1, s1, s2), find(i+1, j, s1, s2));
+            return dp[i][j] = max(find(i, j+1, s1, s2, dp), find(i+1, j, s1, s2, dp));
         }
     }
     int LongestRepeatingSubsequence(string &s) {
         // Code here
         n = s.size();
+        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
         
-        return find(0,0,s,s);
+        return find(0,0,s,s, dp);
     }
+    // int LongestRepeatingSubsequence(string &s) {
+    //     // Code here
+    //     n = s.size();
+        
+    //     return find(0,0,s,s);
+    // }
 };
-
 int main(){
     string s1 = "axxzxy";
     string s2 = "axxxy";
