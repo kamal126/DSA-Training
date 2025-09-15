37da17e049be87feb29ce267b10e5e8b0088c7b8
kamal126 <kamalchandra265@gmail.com>
2025-09-15 11:58:28 +0530

    feat: implement dynamic programming for longest palindromic subsequence

--- DP/09_longestPalindromicSubsequence.cpp
+++ DP/09_longestPalindromicSubsequence.cpp
@@ -15,16 +15,36 @@ class Solution {
             return dp[i][j] = max(find(i-1,j, s1, s2, dp), find(i,j-1, s1, s2, dp));
         }
     }
-    
+
     int longestPalinSubseq(string &s) {
         // code here
         int n = s.size();
-        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+        vector<vector<int>>dp(n+1, vector<int>(n+1, 0));
         string rev = s;
         reverse(rev.begin(), rev.end());
         
-        return find(n-1,n-1,s,rev, dp);
+        for(int i=1;i<=n; i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1] == rev[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[n][n];
     }
+
+    // int longestPalinSubseq(string &s) {
+    //     // code here
+    //     int n = s.size();
+    //     vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+    //     string rev = s;
+    //     reverse(rev.begin(), rev.end());
+        
+    //     return find(n-1,n-1,s,rev, dp);
+    // }
   
     // int longestPalinSubseq(string &s) {
     //     // code here
