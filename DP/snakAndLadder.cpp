b739ca8b528652f8752756cc8c87ac5f376c147c
kamal126 <kamalchandra265@gmail.com>
2025-12-24 22:12:27 +0530

    fix: refactor minThrow function in snakAndLadder

--- DP/snakAndLadder.cpp
+++ DP/snakAndLadder.cpp
@@ -2,56 +2,93 @@
 using namespace std;
 
 
-int snakAndLadder(int N, int arr[])
-{
-    const int SIZE = 30;
-    vector<int> move(SIZE, -1);
+// int snakAndLadder(int N, int arr[]){
+//     const int SIZE = 30;
+//     vector<int> move(SIZE, -1);
 
-    // Build snakes & ladders
-    for (int i = 0; i < 2 * N; i += 2)
-    {
-        move[arr[i]] = arr[i + 1];
-    }
+//     // Build snakes & ladders
+//     for (int i = 0; i < 2 * N; i += 2)
+//     {
+//         move[arr[i]] = arr[i + 1];
+//     }
 
-    vector<bool> visited(SIZE, false);
-    queue<pair<int, int>> q;
+//     vector<bool> visited(SIZE, false);
+//     queue<pair<int, int>> q;
 
-    visited[0] = true;
-    q.push({0, 0});
+//     visited[0] = true;
+//     q.push({0, 0});
 
-    while (!q.empty())
-    {
-        int currPos = q.front().first;
-        int steps = q.front().second;
-        q.pop();
+//     while (!q.empty())
+//     {
+//         int currPos = q.front().first;
+//         int steps = q.front().second;
+//         q.pop();
 
-        if (currPos == SIZE - 1)
-        {
-            return steps;
+//         if (currPos == SIZE - 1)
+//         {
+//             return steps;
+//         }
+
+//         for (int i = 1; i <= 6 && currPos + i < SIZE; i++)
+//         {
+//             int nextPos = currPos + i;
+//             int dest = (move[nextPos] != -1) ? move[nextPos] : nextPos;
+
+//             if (!visited[dest])
+//             {
+//                 visited[dest] = true;
+//                 q.push({dest, steps + 1});
+//             }
+//         }
+//     }
+
+//     return -1;
+// }
+
+
+    int minThrow(int N, int arr[]) {
+        const int SIZE = 30;
+        vector<int> move(SIZE+1, -1);
+
+        // Build snakes & ladders
+        for(int i = 0; i < 2*N; i += 2) {
+            move[arr[i]] = arr[i+1];
         }
 
-        for (int i = 1; i <= 6 && currPos + i < SIZE; i++)
-        {
-            int nextPos = currPos + i;
-            int dest = (move[nextPos] != -1) ? move[nextPos] : nextPos;
+        vector<bool> visited(SIZE+1, false);
+        queue<pair<int,int>> q; // {position, throws}
 
-            if (!visited[dest])
-            {
-                visited[dest] = true;
-                q.push({dest, steps + 1});
+        visited[1] = true;
+        q.push({1, 0});
+
+        while(!q.empty()) {
+            int curr = q.front().first;
+            int throws = q.front().second;
+            q.pop();
+
+            if(curr == 30)
+                return throws;
+
+            for(int j = 1; j <= 6 && curr+j <= 30; j++) {
+                int nextPos = curr + j;
+                int dest = (move[nextPos] != -1) ? move[nextPos] : nextPos;
+
+                if(!visited[dest]) {
+                    visited[dest] = true;
+                    q.push({dest, throws + 1});
+                }
             }
         }
-    }
 
-    return -1;
-}
+        return -1;
+    }
 
 int main()
 {
     int N = 8;
     int arr[] = {3, 22, 5, 8, 11, 26, 20, 29, 17, 4, 19, 7, 27, 1, 21, 9};
 
-    cout << snakAndLadder(N, arr);
+    cout << minThrow(N, arr);
 
     return 0;
 }
